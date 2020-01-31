28aef0a192a431773cc5e3c0885d51468b3703b7
xiaxuantan <xx.peter.tan@gmail.com>
2020-01-30 23:57:06 -0800

    change to hash router

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<HashRouter basename='/'><App /></HashRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
