8abf7cd8068d8d20062d14803299d9f7cf9e6cca
xiaxuantan <xx.peter.tan@gmail.com>
2020-01-31 14:26:45 -0800

    change some styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,6 +4,9 @@ import './About.css';
 const about = () => {
     return (
         <div className="about-bg">
+            <div className="about-content">
+                
+            </div>
         </div>
     )
 }
--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Card } from "react-bootstrap";
+import './Footer.css';
 
 const Footer = (props) => {
 
     return (
-        <Card className="text-center border-0">
-            <Card.Footer style={{ color: "grey", fontSize: "small" }}>
-                Made by <a href="https://reactjs.org/">React</a>.
-                Contact me via <a href="mailto:xx.peter.tan@gmail.com">this address</a>
+        <Card className="footerFrame text-center border-0">
+            <Card.Footer style={{ color: "white", fontSize: "small" }}>
+                Made by <a className="footerLink" href="https://reactjs.org/">React</a>.
+                Contact me via <a className="footerLink" href="mailto:xx.peter.tan@gmail.com">this address</a>
             </Card.Footer>
         </Card>
     )
